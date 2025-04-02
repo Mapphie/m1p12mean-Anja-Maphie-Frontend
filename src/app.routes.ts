cc24f662c82a9f503a2c7af3a1c52666b50d345b
Mapphie <maphiesarobidyrazafindrakotoso@gmail.com>
2025-04-02 16:49:53 +0300

    détails client + devis par client

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -5,7 +5,7 @@ import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
 import { EmployeeComponent } from './app/components/employee/employee.component';
-import { ClientsComponent } from './app/components/clients/clients.component';
+import { ClientsComponent } from './app/components/clients/liste/clients.component';
 import { RendezVousComponent } from './app/components/rendez-vous/rendez-vous.component';
 import { InvoiceListComponent } from './app/components/facture/list/list.component';
 import { DetailComponent } from './app/components/facture/detail/detail.component';
@@ -15,6 +15,7 @@ import { ListComponent } from './app/components/devis/list/list.component';
 import { DevisComponent } from './app/components/devis/details/devis.component';
 import { NewComponent } from './app/components/devis/new/new.component';
 import { UpdateQuoteComponent } from './app/components/devis/update/update.component';
+import { DetailsClientComponent } from './app/components/clients/details/details.component';
 
 export const appRoutes: Routes = [
     {
@@ -27,6 +28,8 @@ export const appRoutes: Routes = [
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
             { path:'employes', component:EmployeeComponent },
             { path:'clients', component:ClientsComponent },
+            { path:'client/:id', component:DetailsClientComponent },
+
             { path:'rendez-vous', component: RendezVousComponent},
 
             { path:'factures', component:InvoiceListComponent },
